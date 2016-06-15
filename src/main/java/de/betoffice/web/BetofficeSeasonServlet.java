ee9f1affad9840f3b723f1620c543b8e2d6b5683
Andre Winkler <gluehloch@googlemail.com>
2016-06-15 21:27:09 +0200

    UPDATE: Calculate user ranking.

--- src/main/java/de/betoffice/web/BetofficeSeasonServlet.java
+++ src/main/java/de/betoffice/web/BetofficeSeasonServlet.java
@@ -192,17 +192,19 @@ public class BetofficeSeasonServlet {
     //
     // user ranking
     //
-    
+
     @RequestMapping(value = "/season/{seasonId}/ranking", method = RequestMethod.GET)
-    public UserTableJson findUserRanking(@PathVariable("seasonId") Long seasonId, HttpServletResponse response) {
+    public @ResponseBody UserTableJson findUserRanking(
+            @PathVariable("seasonId") Long seasonId,
+            HttpServletResponse response) {
 
         ResponseHeaderSetup.setup(response);
         return betofficeBasicJsonService.findUserRanking(seasonId);
     }
 
     @RequestMapping(value = "/season/round/{roundId}/ranking", method = RequestMethod.GET)
-    public UserTableJson findUserTable(@PathVariable("roundId") Long roundId,
-            HttpServletResponse response) {
+    public @ResponseBody UserTableJson findUserTable(
+            @PathVariable("roundId") Long roundId, HttpServletResponse response) {
 
         ResponseHeaderSetup.setup(response);
         return betofficeBasicJsonService.findUserTable(roundId);
