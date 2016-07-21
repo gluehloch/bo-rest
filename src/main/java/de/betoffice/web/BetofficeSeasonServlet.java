917b4dfe9eef365f3d481ebd4aded4d8d41e66d8
Andre Winkler <gluehloch@googlemail.com>
2016-07-21 07:18:12 +0200

    # WARNING: head commit changed in the meantime
    UPDATE: Jackson 2.7

--- src/main/java/de/betoffice/web/BetofficeSeasonServlet.java
+++ src/main/java/de/betoffice/web/BetofficeSeasonServlet.java
@@ -193,21 +193,21 @@ public class BetofficeSeasonServlet {
     // user ranking
     //
 
-    @RequestMapping(value = "/season/{seasonId}/ranking", method = RequestMethod.GET)
+    @RequestMapping(value = "/ranking/season/{seasonId}", method = RequestMethod.GET)
     public @ResponseBody UserTableJson findUserRanking(
             @PathVariable("seasonId") Long seasonId,
             HttpServletResponse response) {
 
         ResponseHeaderSetup.setup(response);
-        return betofficeBasicJsonService.findUserRanking(seasonId);
+        return betofficeBasicJsonService.calcUserRanking(seasonId);
     }
 
-    @RequestMapping(value = "/season/round/{roundId}/ranking", method = RequestMethod.GET)
+    @RequestMapping(value = "/ranking/round/{roundId}/", method = RequestMethod.GET)
     public @ResponseBody UserTableJson findUserTable(
             @PathVariable("roundId") Long roundId, HttpServletResponse response) {
 
         ResponseHeaderSetup.setup(response);
-        return betofficeBasicJsonService.findUserTable(roundId);
+        return betofficeBasicJsonService.calcUserRankingByRound(roundId);
     }
 
     /**
