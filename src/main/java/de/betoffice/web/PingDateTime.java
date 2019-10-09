9c34c67c2db89aa4e09c7c51a86e80510324f63d
andrewinkler <andre.winkler@web.de>
2019-10-09 21:03:37 +0200

    UPDATE: Added Joda DateTime to the ping method.

--- src/main/java/de/betoffice/web/PingDateTime.java
+++ src/main/java/de/betoffice/web/PingDateTime.java
@@ -2,6 +2,8 @@ package de.betoffice.web;
 
 import java.util.Date;
 
+import org.joda.time.DateTime;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
@@ -19,6 +21,8 @@ public class PingDateTime {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd.MM.yyyy HH:mm", locale = "de_DE", timezone = "UTC")
     private Date dateTimeUtc;
 
+    private DateTime jodaDateTime;
+    
     private Date dateTimeMillies;
 
     private String version;
@@ -38,11 +42,16 @@ public class PingDateTime {
     public Date getDateTimeBerlin() {
         return dateTimeBerlin;
     }
+    
+    public DateTime getJodaDateTime() {
+        return jodaDateTime;
+    }
 
     public void setDateTime(Date dateTime) {
         this.dateTimeUtc = dateTime;
         this.dateTimeBerlin = dateTime;
         this.dateTimeMillies = dateTime;
+        this.jodaDateTime = new DateTime(dateTime.getTime());
     }
 
     public Date getDateTimeMillies() {
