6057b83568a8423c212215c881416179877df283
Andre Winkler <andre.winkler@web.de>
2020-11-02 20:00:10 +0100

    WIP: Forbidden access.

--- src/test/java/de/betoffice/web/TippControllerTest.java
+++ src/test/java/de/betoffice/web/TippControllerTest.java
@@ -1,8 +1,13 @@
 package de.betoffice.web;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 
 import javax.transaction.Transactional;
 
@@ -18,6 +23,22 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+import de.betoffice.web.json.SubmitTippRoundJson;
+import de.winkler.betoffice.service.MasterDataManagerService;
+import de.winkler.betoffice.service.SeasonManagerService;
+import de.winkler.betoffice.storage.GameList;
+import de.winkler.betoffice.storage.Group;
+import de.winkler.betoffice.storage.GroupType;
+import de.winkler.betoffice.storage.Season;
+import de.winkler.betoffice.storage.Team;
+import de.winkler.betoffice.storage.User;
+import de.winkler.betoffice.storage.enums.SeasonType;
+
 @ExtendWith(SpringExtension.class)
 // @ContextConfiguration(classes = { PersistenceJPAConfig.class })
 // @ComponentScan("de.betoffice", "de.winkler.betoffice")
@@ -25,29 +46,42 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 @SpringJUnitConfig(locations = { "/betoffice-test-properties.xml", "/betoffice.xml" })
 public class TippControllerTest {
 
+    private static final ZoneId EUROPE_BERLIN = ZoneId.of("Europe/Berlin");
+    private static final LocalDateTime LDT_1971_03_24 = LocalDateTime.of(1971, 3, 24, 20, 30, 0);
+    private static final ZonedDateTime DATE_1971_03_24 = ZonedDateTime.of(LDT_1971_03_24, EUROPE_BERLIN);
+
     private MockMvc mockMvc;
 
     @Autowired
     private BetofficeService betofficeService;
 
-    @BeforeEach
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = MockMvcBuilders
-                .standaloneSetup(new BetofficeController(betofficeService))
-                .build();
-    }
+    @Autowired
+    private MasterDataManagerService masterDataManagerService;
+
+    @Autowired
+    private SeasonManagerService seasonManagerService;
+
+    private GameList round;
 
     @Test
+    @Transactional
     public void ping() throws Exception {
-        mockMvc.perform(get("/office/ping"))
-                .andDo(print())
-                .andExpect(status().isOk());
-        
         mockMvc.perform(get("/office/ping")
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk());
+        
+        SubmitTippRoundJson tipp = new SubmitTippRoundJson();
+        
+        mockMvc.perform(
+                post("/office/tipp/submit")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(toString(tipp))
+                    .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_TOKEN, "token")
+                    .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_NICKNAME, "User A")
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isForbidden());
     }
 
     @Test
@@ -149,20 +183,56 @@ public class TippControllerTest {
         // Matchers.contains("EMAIL_IS_NOT_VALID")));
     }
 
-    // private String toString(RegistrationJson registration) throws
-    // JsonProcessingException {
-    // ObjectMapper mapper = new ObjectMapper();
-    // mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-    // ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-    // String requestJson = ow.writeValueAsString(registration);
-    // return requestJson;
-    // }
-    //
+    private String toString(Object object) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
+        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
+        return ow.writeValueAsString(object);
+    }
+
     // private void setupDatabase() {
     // ApplicationEntity application = new ApplicationEntity();
     // application.setName("application");
     // application.setDescription("Application Description");
     // applicationRepository.save(application);
     // }
+
+    @BeforeEach
+    void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(new BetofficeController(betofficeService))
+                .build();
+
+        Team luebeck = new Team("Vfb Lübeck", "Vfb Lübeck", "luebeck.gif");
+        masterDataManagerService.createTeam(luebeck);
+        Team rwe = new Team("RWE", "Rot-Weiss-Essen", "rwe.gif");
+        masterDataManagerService.createTeam(rwe);
+
+        Season season = new Season();
+        season.setMode(SeasonType.LEAGUE);
+        season.setName("Bundesliga");
+        season.setYear("1999/2000");
+        seasonManagerService.createSeason(season);
+
+        GroupType buli1 = new GroupType();
+        buli1.setName("1. Bundesliga");
+        masterDataManagerService.createGroupType(buli1);
+
+        season = seasonManagerService.addGroupType(season, buli1);
+        Group group = seasonManagerService.findGroup(season, buli1);
+        seasonManagerService.addTeam(season, buli1, rwe);
+        seasonManagerService.addTeam(season, buli1, luebeck);
+
+        round = seasonManagerService.addRound(season, DATE_1971_03_24, buli1);
+        seasonManagerService.addMatch(round, DATE_1971_03_24, group, luebeck, rwe);
+        seasonManagerService.addMatch(round, DATE_1971_03_24, group, rwe, luebeck);
+
+        User userA = new User();
+        userA.setNickname("User A");
+        masterDataManagerService.createUser(userA);
+
+        seasonManagerService.addUser(season, userA);
+    }
 
 }
