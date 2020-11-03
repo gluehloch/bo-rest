6049504169a7fcc88d95ae1408ff0c2624cc755e
Andre Winkler <andre.winkler@web.de>
2020-11-03 19:54:01 +0100

    BUG: Submit tipp after deadline: The service still created a nameless tipp.

--- src/test/java/de/betoffice/web/TippControllerTest.java
+++ src/test/java/de/betoffice/web/TippControllerTest.java
@@ -1,9 +1,10 @@
 package de.betoffice.web;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -34,11 +36,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import de.betoffice.web.json.GameResultJson;
+import de.betoffice.web.json.SubmitTippGameJson;
 import de.betoffice.web.json.SubmitTippRoundJson;
 import de.winkler.betoffice.dao.SessionDao;
 import de.winkler.betoffice.service.MasterDataManagerService;
 import de.winkler.betoffice.service.SeasonManagerService;
 import de.winkler.betoffice.storage.GameList;
+import de.winkler.betoffice.storage.GameTipp;
 import de.winkler.betoffice.storage.Group;
 import de.winkler.betoffice.storage.GroupType;
 import de.winkler.betoffice.storage.Season;
@@ -54,6 +59,7 @@ public class TippControllerTest {
 
     private static final String NICKNAME = "Frosch";
     private static final String PASSWORD = "Password";
+    private static final String USER_AGENT_TEST = "TESTAGENT";
     private static final ZoneId EUROPE_BERLIN = ZoneId.of("Europe/Berlin");
     private static final LocalDateTime LDT_1971_03_24 = LocalDateTime.of(1971, 3, 24, 20, 30, 0);
     private static final ZonedDateTime DATE_1971_03_24 = ZonedDateTime.of(LDT_1971_03_24, EUROPE_BERLIN);
@@ -73,6 +79,7 @@ public class TippControllerTest {
     private SessionDao sessionDao;
     
     private GameList round;
+    private User userA;
 
     @Test
     @Transactional
@@ -88,7 +95,7 @@ public class TippControllerTest {
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(toString(tipp))
                 .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_TOKEN, "token")
-                .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_NICKNAME, "User A")
+                .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_NICKNAME, NICKNAME)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isForbidden());
@@ -100,67 +107,54 @@ public class TippControllerTest {
         ResultActions loginAction = mockMvc.perform(post("/office/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(toString(authenticationForm))
-                .header("User-Agent", "TESTAGENT")
+                .header("User-Agent", USER_AGENT_TEST)
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
-                .andExpect(status().isOk());
-                //.andExpect(header().string("User-Agent", "TESTAGENT"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("token", equalTo("1")))
+                .andExpect(jsonPath("nickname", equalTo(NICKNAME)))
+                .andExpect(jsonPath("role", equalTo("TIPPER")));
 
         MvcResult result = loginAction.andReturn();
         System.out.println(result.getResponse().getContentAsString());
         
         List<Session> session = sessionDao.findByNickname(NICKNAME);
         assertThat(session).hasSize(1);
-        assertThat(session.get(0).getBrowser()).isEqualTo("TESTAGENT");
+        assertThat(session.get(0).getBrowser()).isEqualTo(USER_AGENT_TEST);
         
-//        // Get the JWT from the response header ...
-//        String authorizationHeader = loginAction.getResponse().getHeader(SecurityConstants.HEADER_STRING);
-//        String jwt = authorizationHeader.replace(SecurityConstants.TOKEN_PREFIX, "");
-//        // ... and validate the token.
-//        Optional<String> validate = loginService.validate(jwt);
-//        assertThat(validate).isPresent().get().isEqualTo("Frosch");
-    }
+        tipp.setNickname(NICKNAME);
+        tipp.setRoundId(round.getId());
+        List<SubmitTippGameJson> submitTippGames = new ArrayList<>();
+        SubmitTippGameJson submitTippGame = new SubmitTippGameJson();
+        submitTippGame.setGameId(round.get(0).getId());
+        GameResultJson gameResultJson = new GameResultJson();
+        gameResultJson.setHomeGoals(2);
+        gameResultJson.setGuestGoals(3);
+        submitTippGame.setTippResult(gameResultJson);
+        tipp.setSubmitTippGames(submitTippGames);
 
-    // @Test
-    // @Transactional
-    // public void validRegistration() throws Exception {
-    // setupDatabase();
-    // RegistrationJson registration = new RegistrationJson();
-    // registration.setApplicationName("application");
-    //
-    // mockMvc.perform(post("/registration/register")
-    // .contentType(MediaType.APPLICATION_JSON)
-    // .content(toString(registration)))
-    // .andDo(print())
-    // .andExpect(status().isOk())
-    // .andExpect(jsonPath("validationCodes.*", Matchers.hasSize(6)))
-    // .andExpect(jsonPath("validationCodes.*",
-    // Matchers.containsInAnyOrder("MISSING_ACCEPT_EMAIL", "MISSING_ACCEPT_COOKIE",
-    // "NICKNAME_IS_EMPTY", "PASSWORD_TOO_SHORT", "EMAIL_IS_EMPTY",
-    // "FIRSTNAME_IS_EMPTY")));
-    //
-    // registration.setAcceptCookie(true);
-    // registration.setAcceptMail(true);
-    // registration.setNickname("Frosch");
-    // registration.setFirstname("Andre");
-    // registration.setName("Winkler");
-    // registration.setPassword("secret-password");
-    // registration.setEmail("test@test.de");
-    //
-    // mockMvc.perform(post("/registration/register")
-    // .contentType(MediaType.APPLICATION_JSON)
-    // .content(toString(registration)))
-    // .andDo(print())
-    // .andExpect(status().isOk())
-    // .andExpect(jsonPath("validationCodes", Matchers.hasSize(1)))
-    // .andExpect(jsonPath("validationCodes", Matchers.contains("OK")));
-    //
-    // RegistrationEntity registrationEntity =
-    // registrationRepository.findByNickname("Frosch")
-    // .orElseThrow(() -> new IllegalArgumentException());
-    // assertThat(registrationEntity.getNickname()).isEqualTo("Frosch");
-    // assertThat(registrationEntity.getApplication()).isEqualTo("application");
-    // }
+        ResultActions submitAction = mockMvc.perform(post("/office/tipp/submit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(toString(tipp))
+                .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_TOKEN, "1")
+                .header(BetofficeHttpConsts.HTTP_HEADER_BETOFFICE_NICKNAME, NICKNAME)
+                .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("seasonName", equalTo("Bundesliga")))
+                .andExpect(jsonPath("seasonYear", equalTo("1999/2000")))
+                .andExpect(jsonPath("games[0].homeTeam.name", equalTo("Vfb Lübeck")))
+                .andExpect(jsonPath("games[0].guestTeam.name", equalTo("RWE")))
+                .andExpect(jsonPath("games[0].tipps[0].nickname", equalTo(NICKNAME)));
+        System.out.println(submitAction.andReturn().getResponse().getContentAsString());
+
+        List<GameTipp> tipps = seasonManagerService.findTipps(round, userA);
+        assertThat(tipps).hasSize(1);
+        assertThat(tipps.get(0).getToken()).isEqualTo("1");
+        assertThat(tipps.get(0).getUser().getNickname()).isEqualTo(NICKNAME);
+        assertThat(tipps.get(0).getTipp().getHomeGoals()).isEqualTo(2);
+        assertThat(tipps.get(0).getTipp().getGuestGoals()).isEqualTo(3);
+    }
 
     private String toString(Object object) throws JsonProcessingException {
         ObjectMapper mapper = new ObjectMapper();
@@ -200,7 +194,7 @@ public class TippControllerTest {
         seasonManagerService.addMatch(round, DATE_1971_03_24, group, luebeck, rwe);
         seasonManagerService.addMatch(round, DATE_1971_03_24, group, rwe, luebeck);
 
-        User userA = new User();
+        userA = new User();
         userA.setNickname(NICKNAME);
         userA.setPassword(PASSWORD);
         masterDataManagerService.createUser(userA);
