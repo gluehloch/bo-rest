a1e177da37c6704e9ac736ab302fb42e8d6ef1af
gluehloch <gluehloch@googlemail.com>
2022-12-26 11:05:05 +0100

    Community Edition: fixing...

--- src/main/java/de/betoffice/web/json/CommunityJson.java
+++ src/main/java/de/betoffice/web/json/CommunityJson.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 
 public class CommunityJson extends AbstractIdentifier implements Serializable {
 
+    private static final long serialVersionUID = 1L;
+
     private String name;
     private String shortName;
     private PartyJson communityManager;
