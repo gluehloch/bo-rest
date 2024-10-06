4968e66eb192feb5bf87bae60aa8d6056a9e504a
Andre Winkler <gluehloch@googlemail.com>
2024-10-06 16:27:27 +0200

    WIP: build script to copy war file to productin environment.

---
+++ betoffice-war/build-deploy.sh
@@ -0,0 +1,2 @@
+ #/bin/bash
+ scp .\betoffice-war\target\betoffice-war-1.5.1-SNAPSHOT.war boprod.tdkb2:~/download/betoffice-war-1.5.1-SNAPSHOT-2024-10-06T14:27:03Z.war
---
+++ betoffice-war/src/script/build-deploy.sh
@@ -0,0 +1,2 @@
+ #/bin/bash
+ scp .\betoffice-war\target\betoffice-war-${project.version}.war boprod.tdkb2:~/download/betoffice-war-${project.version}-${maven.build.timestamp}.war
