2ffe1eadbfd646385e7b86d04e7469c151bec6b0
Andre Winkler <gluehloch@googlemail.com>
2024-10-06 19:38:16 +0200

    WIP: build script to copy war file to productin environment. File name and backup unclear.

--- betoffice-war/src/script/build-deploy.sh
+++ betoffice-war/src/script/build-deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+
+# Another possible maven property: ${maven.build.timestamp}
 timestamp=$(date +%s)
-scp ./target/betoffice-war-${project.version}.war boprod.tdkb2:~/download/betoffice-war-${project.version}-${maven.build.timestamp}.war
+scp ./target/betoffice-war-${project.version}.war boprod.tdkb2:~/download/betoffice-war-${project.version}.war
 
 ssh boprod.tdkb2 <<'ENDSSH'
     rm -f ~/webapps/betoffice-war.war
     sleep 15
-    cp ~/download/betoffice-war-${project.version}-${maven.build.timestamp}.war ~/webapps/betoffice-war.war
+    cp ~/download/betoffice-war-${project.version}.war ~/webapps/betoffice-war.war
 ENDSSH
 exit 0;
