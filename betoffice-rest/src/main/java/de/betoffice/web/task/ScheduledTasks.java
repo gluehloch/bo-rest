1f8bdae3704b8c7173646d539b680089ac4cc45e
Andre Winkler <gluehloch@googlemail.com>
2025-01-25 19:14:45 +0100

    WIP: Sending an email...

--- betoffice-rest/src/main/java/de/betoffice/web/task/ScheduledTasks.java
+++ betoffice-rest/src/main/java/de/betoffice/web/task/ScheduledTasks.java
@@ -1,50 +1,62 @@
 package de.betoffice.web.task;
 
 import java.time.ZonedDateTime;
-import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 import de.winkler.betoffice.service.CommunityService;
-import de.winkler.betoffice.service.SeasonManagerService;
 import de.winkler.betoffice.service.TippService;
-import de.winkler.betoffice.storage.Community;
 import de.winkler.betoffice.storage.GameList;
 import de.winkler.betoffice.storage.Season;
 import de.winkler.betoffice.storage.User;
+import de.winkler.betoffice.storage.enums.NotificationType;
 
 @Configuration
 @EnableScheduling
 public class ScheduledTasks {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ScheduledTasks.class);
+
     private final TippService tippService;
     private final CommunityService communityService;
-    private final SeasonManagerService seasonManagerService;
+    private final MailTask mailTask;
 
     public ScheduledTasks(
             final TippService tippService,
             final CommunityService communityService,
-            final SeasonManagerService seasonManagerService) {
+            final MailTask mailTask) {
         this.tippService = tippService;
         this.communityService = communityService;
-        this.seasonManagerService = seasonManagerService;
+        this.mailTask = mailTask;
     }
 
-    //@Scheduled(fixedRate = 5, timeUnit = TimeUnit.SECONDS)
+    @Scheduled(fixedRate = 1, timeUnit = TimeUnit.HOURS)
     public void scheduler() {
         Optional<GameList> nextTippRound = tippService.findNextTippRound(ZonedDateTime.now());
         Season season = nextTippRound.get().getSeason();
 
         Set<User> members = communityService.findMembers(CommunityService.defaultPlayerGroup(season.getReference()));
-        
-        // List<User> users = findUsers(seasonMembers);
-        // Season season = seasonManagerService.findSeasonById(seasonId);
-        // CommunityReference defaultPlayerGroup = CommunityService.defaultPlayerGroup(season.getReference());
-        // tippService.find
+        members.stream().filter(ScheduledTasks::notify).forEach(u -> {
+            try {
+                mailTask.send("betoffice@andre-winkler.de", u.getEmail(), "Spieltag!",
+                        "Heute ist Spieltag. Vergiss deinen Tipp nicht: https://andre-winkler.de");
+            } catch (Exception ex) {
+                LOG.error(String.format("Unable to send an email to %s", u.getEmail()), ex);
+            }
+        });
+
         System.out.println("Hallo. Hallo. Hallo");
+    }
+
+    public static boolean notify(User user) {
+        return NotificationType.TIPP.equals(user.getNotification());
     }
 
 }
