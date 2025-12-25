364c9eaa177d670d6f017881604aac889f9f3d91
Andre Winkler <gluehloch@googlemail.com>
2025-12-25 14:07:34 +0100

    Added some informations to get the system timezone settings.

--- betoffice-springboot/src/main/java/de/betoffice/web/boot/BetofficeBootApplication.java
+++ betoffice-springboot/src/main/java/de/betoffice/web/boot/BetofficeBootApplication.java
@@ -1,5 +1,12 @@
 package de.betoffice.web.boot;
 
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.TimeZone;
+
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -7,8 +14,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
-import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
@@ -19,6 +24,22 @@ public class BetofficeBootApplication {
     private Environment env;
 
     public static void main(String[] args) {
+        // Preferred (Java 8+)
+        ZoneId zoneId = ZoneId.systemDefault();
+        System.out.println("ZoneId.systemDefault(): " + zoneId);
+
+        // java.util.TimeZone
+        TimeZone tz = TimeZone.getDefault();
+        System.out.println("TimeZone.getDefault().getID(): " + tz.getID());
+
+        // System property (may be null or not set)
+        String prop = System.getProperty("user.timezone");
+        System.out.println("System.getProperty(\"user.timezone\"): " + prop);
+
+        // Current offset for the runtime timezone
+        ZoneOffset offset = ZonedDateTime.now(zoneId).getOffset();
+        System.out.println("Current offset: " + offset);
+
         SpringApplication.run(BetofficeBootApplication.class, args);
     }
 
@@ -31,4 +52,5 @@ public class BetofficeBootApplication {
     private String profilesToString(String[] profiles) {
         return String.join(",", profiles);
     }
+
 }
