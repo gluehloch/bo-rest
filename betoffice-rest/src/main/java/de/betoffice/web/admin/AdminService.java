4785d0b550a618581813c23656e113ac1d1a4bb8
Andre Winkler <gluehloch@googlemail.com>
2025-12-27 16:08:11 +0100

    WIP: Removed AccessDeniedException. User-Role-Validation should be done by Spring-Security.

--- betoffice-rest/src/main/java/de/betoffice/web/admin/AdminService.java
+++ betoffice-rest/src/main/java/de/betoffice/web/admin/AdminService.java
@@ -27,7 +27,6 @@ import java.util.List;
 import java.util.Optional;
 
 import de.betoffice.storage.team.TeamType;
-import de.betoffice.web.AccessDeniedException;
 import de.betoffice.web.json.GameJson;
 import de.betoffice.web.json.GroupTypeJson;
 import de.betoffice.web.json.PartyJson;
@@ -45,7 +44,7 @@ import de.betoffice.web.json.TeamJson;
 public interface AdminService {
 
     /**
-     * Validate admin session. Throws {@link AccessDeniedException}.
+     * Validate admin session.
      *
      * @param token the session token
      */
