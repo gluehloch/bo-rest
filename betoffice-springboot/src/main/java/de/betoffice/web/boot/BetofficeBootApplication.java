5fc001983994653676034b7bef021abe61196486
Andre Winkler <gluehloch@googlemail.com>
2025-12-27 11:08:22 +0100

    Added package as war to the spring boot app.

--- betoffice-springboot/src/main/java/de/betoffice/web/boot/BetofficeBootApplication.java
+++ betoffice-springboot/src/main/java/de/betoffice/web/boot/BetofficeBootApplication.java
@@ -12,13 +12,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class })
 @ComponentScan(basePackages = { "de.betoffice" })
-public class BetofficeBootApplication {
+public class BetofficeBootApplication extends SpringBootServletInitializer {
 
     @Autowired
     private Environment env;
