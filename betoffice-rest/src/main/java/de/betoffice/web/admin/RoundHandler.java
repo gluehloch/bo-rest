259e6e9c003c0014ef9617acc4056705439cf61e
gluehloch <gluehloch@googlemail.com>
2026-07-13 07:12:42 +0200

    WIP: Spieltagsverwaltung / Import order

--- betoffice-rest/src/main/java/de/betoffice/web/admin/RoundHandler.java
+++ betoffice-rest/src/main/java/de/betoffice/web/admin/RoundHandler.java
@@ -1,11 +1,9 @@
 package de.betoffice.web.admin;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import de.betoffice.service.MasterDataManagerService;
